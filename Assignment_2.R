33e834b3ab942d1c73d5fba18cda76922832eb34
alexrendelll <arendell@student.unimelb.edu.au>
2022-05-01 01:22:14 +1000

    Defined Priors

--- Assignment_2.R
+++ Assignment_2.R
@@ -103,19 +103,6 @@ round(A_HAT,3)
 round(SIGMA_HAT,3)
 round(cov2cor(SIGMA_HAT),3)
 
-
-# PRIOR DISTRIBUTION COPIED FROM L10
-############################################################
-KAPPA_P_A   = 0.02^2
-KAPPA_P_E   = 100
-A_PRIOR     = matrix(0,K,N)
-V_PRIOR     = diag(c(KAPPA_P_E,KAPPA_P_A*((1:p)^(-2))%x%rep(1,N)))
-V_PRIOR_INV = diag(1/c(KAPPA_P_E,KAPPA_P_A*((1:p)^(-2))%x%rep(1,N)))
-S_PRIOR     = diag(diag(SIGMA_HAT))
-NU_PRIOR    = N+1
-
-
-
 # SPECIFYING HYPER-PARAMETERS OF PRIOR DISTRIBUTION   
 S = c(100,1000)
 a0 = 2                      # TO BE CHANGED?
@@ -130,51 +117,67 @@ colnames(posterior1) = c("A")
 colnames(posterior2) = c("E")
 colnames(posterior3) = c("Ka","Ke")
 
+
+# PRIOR DISTRIBUTION COPIED FROM L10
+############################################################
+KAPPA_P_A             = 0.02^2
+KAPPA_P_E             = 100
+A_MEAN_PRI            = matrix(0, nrow(A_HAT),nrow(A_HAT))
+A_MEAN_PRI[2:11,2:11] = diag(10)
+V_PRIOR               = diag(c(KAPPA_P_E,KAPPA_P_A*((1:p)^(-2))%x%rep(1,N)))   # COL SPECIFIC VAR
+V_PRIOR_INV           = diag(1/c(KAPPA_P_E,KAPPA_P_A*((1:p)^(-2))%x%rep(1,N))) 
+S_PRIOR               = diag(diag(SIGMA_HAT)) 
+NU_PRIOR              = N+1
+
+A_PRIOR               = matrix(0, nrow(A_HAT),nrow(A_HAT))
+SIGMA_PRIOR           = diag(41)
+
 # SPECIFYING HYPER-PARAMETERS
-hyper             = c(1,2,3,3,1,3,1)
-names(hyper)      = c("S_BAR(Ka)","V_BAR(KA)","ALPHA_BAR(Ke)","BETA_BAR(Ke)","A_uBAR","S_BAR", "V_BAR")
-
-
-# STARTING VALUES 
-aux_ka          = 2
-aux_ke          = 2
-aux_A           = matrix(0, K, N)
-aux_E           = rep(1,N)
-aux_E           = diag(aux_E)
+hyper                 = c(1,3,1,3,A_MEAN_PRI,S_PRIOR,V_PRIOR)
+names(hyper)          = c("S_PRIOR_Ka","V_PRIOR_KA","ALPHA_PRIOR_Ke","BETA_PRIOR_Ke_","A_MEAN_PRI","S_PRIOR", "V_PRIOR", "NU_PRIOR")
 
 
 # POSTERIOR DRAWS
-posterior_A     = matrix(NA, S, K)
-posterior_E     = matrix(NA, S, K)
-posterior_ka    = matrix(NA, S)
-posterior_ke    = matrix(NA, S)
+posterior_A           = matrix(NA, S, K)
+posterior_E           = matrix(NA, S, K)
+posterior_ka          = matrix(NA, S)
+posterior_ke          = matrix(NA, S)
 
 for (s in 1:S) {
   
-  # SAMPLE Ke
+  # HYPER-PARAMETER ESTIMATION -- K_E
   alpha_bar_ke    = hyper[3] - ((hyper[7]*N))/2
-  beta_bar_ke     = hyper[4] + 0.5*sum(diag(solve(aux_E)*hyper[6]))
+  beta_bar_ke     = hyper[4] + 0.5*sum(diag(solve(SIGMA_PRIOR)*hyper[6]))
   
-  # SAMPLE Ka
-  s_bar_ka        = as.numeric(sum(diag((solve(aux_E)%*%t(aux_A-hyper[5])%*%(aux_A-hyper[5])) + hyper[1])))
+  # SAMPLING -- K_E
+  KAPPA_P_E       = rgamma(1, shape = alpha_bar_ke, scale = 1/beta_bar_ke)
+  posterior_ke    = KAPPA_P_E
+  
+  
+  # HYPER-PARAMETER ESTIMATION -- K_A
+  s_bar_ka        = as.numeric(sum(diag((solve(SIGMA_PRIOR)%*%t(A_PRIOR-hyper[5])%*%(A_PRIOR-hyper[5])) + hyper[1])))
   v_bar_ka        = hyper[2] + N*K
   
+  # SAMPLING -- K_A
+  KAPPA_P_A       = s_bar_ka/rchisq(1, v_bar_ka)
+  posterior_ka    = KAPPA_P_A
+  
   # PARAMETERS OF MVNIW POSTERIOR
-  V_bar_inv       = crossprod(X) + 1/(aux_ka*hyper[7])
-  V_bar_inv_chol  = chol(V_bar_inv)
-  V_bar           = as.numeric(solve(V_bar_inv))
-  A_bar           = as.numeric(V_bar %*% (t(X)%*%Y) + solve(aux_ka*V_bar) %*% hyper[5])
-  s_bar           = as.numeric(crossprod(Y) + aux_ke*hyper[6] + t(aux_A)*solve((aux_ka*hyper[7]))*aux_A + t(A_bar)%*%solve(V_bar)%*%A_bar )
+  V_bar_inv       = crossprod(X) + V_PRIOR_INV*(1/KAPPA_P_A)
+  V_bar           = solve(V_bar_inv)
   
-  # SAMPLE -- aux_ka
-  aux_ka          = s_bar_ka/rchisq(1, v_bar_ka)
-  posterior_ka    = aux_ka
+  #WORKING
+  #V_bar_inv       = crossprod(X) + V_PRIOR_INV*(1/KAPPA_P_A)
+  #V_bar_inv_chol  = chol(V_bar_inv)
+  #A_bar_temp      = t(X)%*%Y + diag(1/diag(V_PRIOR*KAPPA_P_A))*hyper[5]
+
+  A_bar           = V_bar%*% ( t(X)%*%Y + diag(1/diag(V_PRIOR*KAPPA_P_A))*hyper[5])
+  s_bar           = crossprod(Y) + KAPPA_P_E*hyper[6] + t(hyper[5])*solve((KAPPA_P_A*hyper[7]))*hyper[5] + t(A_bar)%*%solve(V_bar)%*%A_bar )
+  nu_bar          = T + NU_PRIOR
   
-  # SAMPLE -- aux_ke
-  aux_ke          = rgamma(1, shape = alpha_bar_ke, scale = 1/beta_bar_ke)
-  posterior_ke    = aux_ke
-  
-  
+xxx = V_bar_inv
+ncol(xxx)
+nrow(xxx)
   # SAMPLE -- aux_A & aux_E
   aux_E           = rWishart(S, df = V_bar, Sigma = s_bar)
   aux_E           = apply(aux_E, 3, solve)
@@ -205,6 +208,7 @@ return(
   )
 )
 }
+
 
 
 # QUESTION 9 
