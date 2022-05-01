37dad556907e8e04d86c9207be6a0d31f6b1fe02
alexrendelll <arendell@student.unimelb.edu.au>
2022-05-01 15:05:32 +1000

    Sampler Code!
    Updated

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,3 +1,4 @@
+# QUESTION 6
 # INSTALLING REQUIRED PACKAGES
 library(readrba)
 library(readabs)
@@ -6,12 +7,9 @@ library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(reticulate)
+library(mvtnorm)
 search(gsl)
-
-rm(list=ls()) #remove current memory of the environment
-
-
-# EXERCISE 1 - DATA COLLECTION AND TRANSFORMING
+# DATA COLLECTION AND TRANSFORMING
 ############################################################
 
 HOURS               = read_abs(series_id ="A2304428W")
@@ -30,7 +28,7 @@ CPI                 = read_abs(series_id = "A3604506F")
 CPI                 = CPI %>%slice(-c(1:145))
 CPI                 = CPI[c(4,6)]
 
-VACANCIES           = read_abs(series_id = "A590698F") #excluded from analysis
+VACANCIES           = read_abs(series_id = "A590698F")
 VACANCIES           = VACANCIES %>%slice(-c(1:22))
 VACANCIES           = VACANCIES[c(4,6)]
 
@@ -63,10 +61,10 @@ TOT                 = TOT[c(4,6)]
 N                   = 10
 p                   = 4
 K                   = 1+p*N
-S                   = 1000
+S                   = 100
 h                   = #TBD
   
-TT                  = nrow(Y)
+  TT                  = nrow(Y)
 T                   = TT - 1
 
 # Create Y and X
@@ -92,11 +90,8 @@ X                   = cbind(X,
                             y[5:nrow(y)-2,],
                             y[5:nrow(y)-3,],
                             y[5:nrow(y)-4,]
+)
 
-                                                       )
-#plot times series data and comment on the plot
-plot(y)
-          
 ##GRAPHING CODE
 #GermanGNP           = ts(as.data.frame(read.csv("GermanGNP.csv")), start=c(1975,1), frequency=4, names="GermanGNP")
 #GerGNP              = matrix(GermanGNP)
@@ -110,7 +105,7 @@ round(A_HAT,3)
 round(SIGMA_HAT,3)
 round(cov2cor(SIGMA_HAT),3)
 
-# PRIOR DISTRIBUTION DEFINITION
+# PRIOR DISTRIBUTION COPIED FROM L10
 ############################################################
 KAPPA_P_A             = 100
 KAPPA_P_E             = 0.02^2
@@ -138,23 +133,23 @@ posterior_ka          = matrix(NA, S)
 posterior_ke          = matrix(NA, S)
 
 GIBBS_SAMPLER = function(S, Y, X , hyper){
-
+  
   for (s in 1:S){
     # HYPER-PARAMETER ESTIMATION -- K_E
     alpha_bar_ke        = hyper$ALPHA_PRIOR_Ke + ((hyper$V_PRIOR_KA*N))/2
     beta_bar_ke         = solve(hyper$BETA_PRIOR_Ke) + 0.5*sum(diag(solve(SIGMA_PRIOR)%*%hyper$S_PRIOR))
-  
+    
     # SAMPLING -- K_E
     KAPPA_P_E           = rgamma(1, shape = alpha_bar_ke, scale = beta_bar_ke)
-  
+    
     help(rgamma)
     # HYPER-PARAMETER ESTIMATION -- K_A
     s_bar_ka            = sum(diag((solve(SIGMA_PRIOR)%*%t(A_PRIOR-hyper$A_MEAN_PRI))%*%(A_PRIOR-hyper$A_MEAN_PRI))) + hyper$S_PRIOR_Ka
     v_bar_ka            = hyper$V_PRIOR_KA + N*K
-  
+    
     # SAMPLING -- K_A
     KAPPA_P_A           = s_bar_ka/rchisq(1, v_bar_ka)
-  
+    
     # PARAMETERS OF MVNIW POSTERIOR
     V_bar_inv           = crossprod(X) + solve(KAPPA_P_A*hyper$V_PRIOR)
     V_bar               = solve(V_bar_inv)
@@ -166,21 +161,21 @@ GIBBS_SAMPLER = function(S, Y, X , hyper){
     #S_bar_chol         = chol(S_bar)
     #S_bar_inv          = backsolve(S_bar_chol, forwardsolve(t(S_bar_chol), diag(N)))
     nu_bar              = T + NU_PRIOR
-
+    
     # SAMPLE -- aux_A & aux_E
     L                   = t(solve(chol(V_bar_inv)))
     SIGMA_POST          = solve(rWishart(1, df = nu_bar, Sigma =S_bar_inv)[,,1])
     draw.norm           = array(rnorm(prod(N*K)),c(K,N))
     A_POST              = A_bar + L%*%draw.norm%*% chol(SIGMA_POST)
-          
+    
     round(apply(A_POST,1:2, mean),3)
-                          
-    posterior_ka[s] = KAPPA_P_A
-    posterior_ke[s] = KAPPA_P_E
-    posterior_A[s,,] = A_POST
-    posterior_E[,,s] = SIGMA_POST
+    
+    posterior_ka[s]     = KAPPA_P_A
+    posterior_ke[s]     = KAPPA_P_E
+    posterior_A[s,,]    = A_POST
+    posterior_E[,,s]    = SIGMA_POST
   }
-
+  
   # OUTPUT  
   return(
     list(
@@ -193,19 +188,16 @@ GIBBS_SAMPLER = function(S, Y, X , hyper){
 }
 
 
+SAMPLER_OUTPUT = GIBBS_SAMPLER(S, Y , X, hyper)
+
 
 # QUESTION 9 
 # SAMPLE -- PREDICTIVE DENSITY 
-h               = 14
-L               = t(solve(Chol(V_bar_inv)))
-Y.h             = array(NA,c(h,N.S))
+h               = 1
+Y.h             = array(NA,c(h,2,S))
+Y.h.m           = array(NA,c(h,2))
 
 for (s in 1:S){
-  cat(s)
-  draw_ norm    = array(mvrnorm::rnorm(N*K),c(K,N))
-  posterior_E   = solve rWishart(1, df= nu_bar, Sigam=S_bar_inv)[,,1]
-  posterior_A   = A_bar + L %*%draw.norm%*%chol(posterior_E)
-  
   if (p==1){
     x.Ti          = matrix(Y[(nrow(Y)-p+1):nrow(Y),],nrow=1)
     x.Ti.m        = x.Ti
@@ -214,13 +206,11 @@ for (s in 1:S){
     x.Ti          = x.Ti[p:1,]
     x.Ti.m        = x.Ti
   }
-  
-  
   for (i in 1:h){
     x.T           = c(1,as.vector(t(x.Ti)))
     x.T.m         = c(1,as.vector(t(x.Ti.m)))
-    Y.f           = rmvnorm(1, mean = x.T%*%posterior_A, sigma=posterior_E)
-    Y.f.m         = x.T.m%*%A.bar
+    Y.f           = rmvnorm(1, mean = x.T%*%SAMPLER_OUTPUT$A[s,,], sigma=SAMPLER_OUTPUT$E[,,s])
+    Y.f.m         = x.T.m%*%A_POST
     if (p==1){
       x.Ti        = Y.f
       x.Ti.m      = Y.f.m
@@ -230,19 +220,6 @@ for (s in 1:S){
     }
     Y.h[i,,s]     = Y.f[1:2]
     Y.h.m[i,]     = Y.f.m[1:2]
-  }
-}
-  
-  
-  
-  x.Ti          = Y[(nrow(Y)-h+1):nrow(Y),]
-  x.Ti          = x.Ti[4:1,]                  #MAY NEED TO CHANGE DIMENSIONS
-  for (i in 1:h){
-    x.T         = c(1,as.vector(t(x.Ti)))
-    Y.h[i,,s]   = rmvnorm(1,
-                          mean  = x.T%*%posterior_A[,,s],
-                          sigma = posterios_E[,,s])
-    x.Ti        = rbind(Y.h[i,,s], x.Ti[1:3,])
   }
 }
 
